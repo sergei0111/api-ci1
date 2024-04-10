60760e419bc4166e28fcbe48df46f842f859a0d6
sergei <osp11011983@mail.ru>
2024-04-10 12:27:10 +0200

    added new schema.json

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,9 +1,11 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.hasSize;
 
 class MobileBankApiTestV1 {
     @Test
@@ -18,6 +20,9 @@ class MobileBankApiTestV1 {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3));
     }
 }
