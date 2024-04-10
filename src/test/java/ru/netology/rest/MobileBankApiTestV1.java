cc0365d7b3a097abb97c109fe3b146e577ef1483
sergei <osp11011983@mail.ru>
2024-04-10 12:42:32 +0200

    code change

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,12 +1,9 @@
 package ru.netology.rest;
-
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.hasSize;
-
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
